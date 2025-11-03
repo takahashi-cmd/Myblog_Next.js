e87cd9cf4cc697479df93d5d10228da824a26b19
hiro_53 <194663299+takahashi-cmd@users.noreply.github.com>
2025-11-03 22:49:33 +0900

    記事投稿機能、削除機能を追加

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
 import { getDetailArticle } from "@/app/blogAPI";
+import DeleteButton from "@/app/components/DeleteButton";
 
 const Article = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const detailArticle = await getDetailArticle(id);
-  console.log(detailArticle)
+  console.log(detailArticle);
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
@@ -19,6 +20,9 @@ const Article = async ({ params }: { params: Promise<{ id: string }> }) => {
       </h1>
       <div className="text-lg leading-relaxed text-justify">
         <p>{detailArticle.content}</p>
+      </div>
+      <div className="text-right mt-3">
+        <DeleteButton id={detailArticle.id} />
       </div>
     </div>
   );
