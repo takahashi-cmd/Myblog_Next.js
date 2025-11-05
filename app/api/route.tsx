9a5affb05159c62c6ae411377d3a731fc1c35bae
hiro_53 <194663299+takahashi-cmd@users.noreply.github.com>
2025-11-05 23:50:56 +0900

    supabaseからAPI取得で記事内容を表示

---
+++ app/api/route.tsx
@@ -0,0 +1,11 @@
+import { supabase } from "@/app/utils/supabaseClient";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  const { data: posts, error } = await supabase.from("posts").select("*");
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  return NextResponse.json(posts);
+}
