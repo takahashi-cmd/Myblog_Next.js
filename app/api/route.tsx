50c4d809dbf4808fa3251c3017115fcb1c54546d
hiro_53 <194663299+takahashi-cmd@users.noreply.github.com>
2025-11-06 22:44:03 +0900

    supabaseを利用して詳細ブログページの表示機能を追加

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -2,7 +2,7 @@ import { supabase } from "@/app/utils/supabaseClient";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request:NextRequest) {
   const { data: posts, error } = await supabase.from("posts").select("*");
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -5,8 +5,11 @@ import DeleteButton from "@/app/components/DeleteButton";
 
 const Article = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  const detailArticle = await getDetailArticle(id);
-  console.log(detailArticle);
+  // const detailArticle = await getDetailArticle(id);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/${id}`, { next: { revalidate: 10 } });
+  const detailArticle = await res.json();
+
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
